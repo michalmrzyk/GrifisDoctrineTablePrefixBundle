bb636890ab03fca11dd9ecfc06b32cac3af7613b
GrifiS <dev@localhost>
2012-09-26 03:33:21 +0300

    update composer.json, add symfony/yaml

--- DependencyInjection/GrifisDoctrineTablePrefixExtension.php
+++ DependencyInjection/GrifisDoctrineTablePrefixExtension.php
@@ -5,7 +5,7 @@ namespace Grifis\DoctrineTablePrefixBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +22,7 @@ class GrifisDoctrineTablePrefixExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
